9847bfadcc47781d1e8681d3ea0f9f3c57d33872
marufdsi <maruf.csea1@gmail.com>
2021-11-24 04:21:53 -0500

    results are updated

--- build/cascade_job.sh
+++ build/cascade_job.sh
@@ -26,7 +26,7 @@ arch=2
 it=25
 
 echo "Path: "$1" threads: "$2" io Method: "$3
-for v in 4 5 6 #0 1 2 3
+for v in 4 5 0 3 #6 0 1 2 3
 #for v in 0 1 2 3
 do
 #	perf stat -B -e cycles:uk,cache-misses:uk,branches:uk,branch-misses:uk,cpu-clock:uk,alignment-faults:uk,cs:uk,L1-dcache-load-misses:uk,L1-dcache-store-misses:uk 
