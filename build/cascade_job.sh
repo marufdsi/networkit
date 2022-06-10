15671e6b83fca1ca36ec8ea8293ac48f2bedded8
marufdsi <maruf.csea1@gmail.com>
2022-06-10 15:41:56 -0400

    Stable nodes results are added

--- build/cascade_job.sh
+++ build/cascade_job.sh
@@ -26,8 +26,7 @@ arch=2
 it=25
 
 echo "Path: "$1" threads: "$2" io Method: "$3
-for v in 4 5 0 3 #6 0 1 2 3
-#for v in 0 1 2 3
+for v in 5 6
 do
 #	perf stat -B -e cycles:uk,cache-misses:uk,branches:uk,branch-misses:uk,cpu-clock:uk,alignment-faults:uk,cs:uk,L1-dcache-load-misses:uk,L1-dcache-store-misses:uk 
 	./networkit_tests $1 $2 $v $3 $it $vecType $arch
